f3fcee5d9dfd2c9cc5b04bd3ff0e519196ce1c1d
jose <josed.escobar@outlook.com>
2021-08-10 13:41:17 -0500

    se agregaron ejemplos de ciclos en go

---
+++ go/src/loops/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// For condicional clasico.
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+
+	// For while.
+
+	counter := 0
+
+	for counter < 10 {
+		fmt.Println(counter)
+		counter++
+	}
+
+	// For forever.
+
+	counter_forever := 0
+
+	for {
+		fmt.Println(counter_forever)
+		counter_forever++
+	}
+
+}
